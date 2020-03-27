283a5297c00ea87389be8d71c41c3023504c1c09
柴建祥 <chaijianxiang@lanjingren.com>
2020-03-27 15:52:53 +0800

    提供下载功能

--- constance.js
+++ constance.js
@@ -1,5 +1,7 @@
-const { version } = require('../package.json')
+// const { version } = require('../package.json')
+
+const downloadDirectory = `${process.env[process.platform === 'darwin' ? 'HOME' : 'USERPROFILE']}/.template`
 
 module.exports = {
-  version
+  downloadDirectory
 }
--- create.js
+++ create.js
@@ -1,19 +1,51 @@
+const path = require('path');
 const axios = require('axios')
 const ora = require('ora')
 const Inquirer = require('inquirer')
+const { promisify } = require('util');
+const downloadGitRepo = require('download-git-repo')
+let ncp = require('ncp');
+ncp = promisify(ncp);
+
+
+const downloadDirectory = require('./constance');
+
+const download = async (repo, tag) => {
+  let api = `Lpeanut/${repo}`
+  if (tag) {
+    api += `#${tag}`
+  }
+  console.log(repo, tag)
+  const dest = `${downloadDirectory}/${repo}`
+  await downloadGitRepo(api, dest)
+  return dest
+}
 
 // 封装loading效果
 const waitFnloading = (fn, message) => async(...args) => {
   const spinner = ora(message)
   spinner.start()
-  const result = await fn(...args)
-  spinner.succeed()
-  return result;
+  try {
+    let result = await fn(...args)
+    spinner.succeed()
+    return result;
+  } catch (error) {
+    console.log('error')
+    spinner.succeed()
+    return ['hhh']
+  }
+  // const result = await fn(...args)
+  // spinner.succeed()
+  // return result;
 }
 
 const fetchTagList = async () => {
-  const { data } = await axios.get('https://api.github.com/repos/Lpeanut/Fina/branch');
-  return data;
+  try {
+    const { data } = await axios.get('https://api.github.com/repos/Lpeanut/tot/tags');
+    return data;
+  } catch (error) {
+    return []
+  }
 }
 
 const fetchRepoList = async () => {
@@ -24,6 +56,7 @@ const fetchRepoList = async () => {
 
 module.exports = async (projectName) => {
   let repos = await waitFnloading(fetchRepoList, 'fetching template')()
+  console.log(repos)
   repos = repos.map(item => item.name)
   const { repo } = await Inquirer.prompt({
     name: 'repo',
@@ -32,12 +65,15 @@ module.exports = async (projectName) => {
     choices: repos
   })
   let tags = await waitFnloading(fetchTagList, 'fetching tags')(repo)
+  tags = tags.map(item => item.name)
   const { tag } = await Inquirer.prompt({
     name: 'repo',
     type: 'list',
     message: 'please choise a tag',
     choices: tags
   })
-  tags = tags.map(item => item.name)
-  console.log('tags', tags)
+  // const result = await download(repo, tag)
+  console.log('repo, tag', repo, tag)
+  const result = await waitFnloading(download, 'do') (repo, tag)
+  ncp(result, path.resolve(projectName))
 }
